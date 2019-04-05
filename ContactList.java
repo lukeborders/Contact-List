62844a0260601b21e1cdfd3d873c4cec616306aa
Luke Borders <31492762+lukeborders@users.noreply.github.com>
2019-04-05 08:58:20 -0700

    Update ContactList.java

--- ContactList.java
+++ ContactList.java
@@ -1,42 +1,40 @@
 import java.util.Scanner;
 import java.util.Collections.sort;
 public class ContactList extends Contact {
+	private String name;
+	private int pNum;
+		
 	public static void main(String[] args) {
-		ArrayList<Contact> list = new ArrayList<Contact>(); // new arraylist
-		list = Collections.sort(list); 		// sort the arraylist in alphabetical order
-	}
-	public void viewList(ArrayList<Contact> list,Scanner scanner) {
-			if(scanner.nextLine().equals("view")) {
-			for(Contact a: list) {
-				System.out.println(a);
-			} 
-		}
-	}
-	public void addContact(Scanner scanner) {
-		//Scanner scanner = new Scanner(System.in); // creates scanner object
-		Char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',',','.','\''} 
-		if(scanner.nextLine().equals("add contact")) {
-			System.out.println("Please Input Name:");
-			String n = scanner.nextLine();
-			for(int i = 0; i < n.length();i++) {
-				for(int m = 0; m < alphabet.length, m++;) {
+		String c1 = "";
+		int c2 = 0; 
+		Scanner scan = new Scanner(System.in);
+		ArrayList<Contact> list;// new arraylist
+		sort(list);
+		if(scan.nextLine().equals("add contact")) {
+			Char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+			System.out.println("Please Input the contact name:");
+			for(int i = 0; i < na.length();i++) {
+				for(int m = 0; m < alphabet.length; m++) {
 					if(n.charAt(i) != alphabet[m] ) {
 						System.out.println("invalid input, try again:");
-						i=n.length();
-						n = scanner.nextLine();
+						i=na.length();
+						na = scan.nextLine();
 					}		
 				}
 			}
-			System.out.println("Please Input Phone Number:");
-			int num = scanner.nextInt();
-			Contact newContact = new Contact(n,num);
+			c1 = scan.nextLine();
+			c2 = scan.nextInt();
+			Contact newContact = new Contact(c1,c2);
 			list.add(newContact);
-			Collections.sort(list);
 		}
-	}
-	public static void exit(Scanner scanner) {
-		if(scanner.nextLine().equals("stop")) {
+		if(scan.nextLine().equals("view")) {
+			for(Contact a: list) {
+				System.out.println(a);
+			}
+		}
+		if(scan.nextLine().equals("stop")) {
 			System.exit(0);
 		}
+		
 	}
 }
