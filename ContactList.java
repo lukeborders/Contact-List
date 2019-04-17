ecb52f6e71cbe380ec0046f05a971bd4dfdd7cbd
Luke Borders <31492762+lukeborders@users.noreply.github.com>
2019-04-16 21:37:43 -0700

    Update ContactList.java

--- ContactList.java
+++ ContactList.java
@@ -11,9 +11,8 @@ public class ContactList {
 		int c2 = 0;
 		Scanner scan = new Scanner(System.in); // new scanner object
 		ArrayList<Contact> list = new ArrayList<>();// new arraylist
-		System.out.println("Enter Command:");
 		String command = scan.nextLine();
-		String[] commands = {"view: shows list of your contacts in alphabetical order","add contact: adds a contact to your list of contacts","stop: exits the program"};
+		String[] commands = {"view: shows list of your contacts in alphabetical order","add contact: adds a contact to your list of contacts","edit: allows the user to edit a specified contact","stop: exits the program"};
 		String[] contacts = {}; //new array of type String
         int editNum = 0;
 		String newnumstring = "";
@@ -22,10 +21,11 @@ public class ContactList {
             if (command.equals("add contact")) {
                 System.out.println("Please Input the contact name:");
                 c1 = scan.nextLine();
-                ArrayList<Character> alphabet = new ArrayList<Character>(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'));
+                ArrayList<Character> alphabet = new ArrayList<Character>(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'));
+                ArrayList<Integer> numberList = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,0));
                 for(int i = 0; i < c1.length();i++) {
                 	if(!alphabet.contains(c1.charAt(i))) {
-                		System.out.println("Invalid Input");
+                		System.out.println('\n'+"Invalid Input: unexpected character");
                 	}
                 	break;
                 }
@@ -33,58 +33,82 @@ public class ContactList {
                 c2 = scan.nextInt();
 				newnumstring = Integer.toString(c2);
                 if(Integer.toString(c2).length() != 10) {
-                    System.out.println("Invalid Input: Phone Number must be 10 integers");
+                    System.out.println('\n' + "Invalid Input: Phone Number must be 10 integers");
                 }
                 else {
+                    for(int m = 0; m < newnumstring.length();m++) {
+                        if(!numberList.contains(newnumstring.charAt(m))) {
+                            System.out.println("Invalid Input: unexpected character");
+                        }
+                        break;
+                    }
                     Contact newContact = new Contact(c1, c2); // create new contact to add to the ArrayList
                     list.add(newContact);
-                    System.out.println("Contact Successfully Added");
+                    System.out.println('\n'+"Contact Successfully Added");
+                    
                 }
+                  
             } else if (command.equals("view")) {
                 contacts = new String[list.size()];
-                for (int i = 0; i < list.size(); i++) { //loops through the entire size of list (in list elements)
-                    contacts[i] = "#" + Integer.toString(i) + " " + "Contact Name: " + c1 + " " + "Contact Phone Number: " + Integer.toString(c2);
-
+                if(list.size() > 0) {
+                    for (int i = 0; i < list.size(); i++) { //loops through the entire size of list (in list elements)
+                        contacts[i] = "#" + Integer.toString(i) + " " + list.get(i).toString();
+                    }
+                    for (String a : contacts) {
+                        System.out.println(a); //prints every element in contacts[]
+                    }
                 }
-
-                for (String a : contacts) {
-                    System.out.println(a); //prints every element in contacts[]
+                else {
+                 System.out.println('\n'+"You need to create a contact in your list before you can do this.");   
                 }
-            } else if (command.equals("show commands")) {  // shows all available commands to the use
-                for (String a : commands) {
-                    System.out.println(a);
+                
+            } else if (command.equals("show commands")) {  // shows all available commands to the user
+                    for (String a : commands) {
+                        System.out.println(a);
+                    }
                 }
-            }
             else if(command.equals("edit")) {
-                System.out.println("Please select the number contact you want to edit:");
-                for (String a : contacts) {
-                    System.out.println(a); //prints every element in contacts[]
+                if(contacts.length < 0) {
+                    for (int g = 0; g < list.size(); g++) { //loops through the entire size of list (in list elements)
+                        contacts[g] = "#" + Integer.toString(g) + " " + list.get(g).toString();
+                    }
                 }
-                editNum = scan.nextInt();
-				System.out.println(list.get(editNum));
-				System.out.println("Do you need to edit the username? Y/N");
-				if(command.equals("Y")||command.equals("y")) {
-					System.out.println("Type in new name:");
-					c1= scan.nextLine();
-				}
-				else if(command.equals("N")||command.equals("n")) {
-					System.out.println("Do you need to edit the phone number? Y/N");
-					if(command.equals("Y")||command.equals("y")) {
-						System.out.println("Type in new phone number");
-						c2 = scan.nextInt();
-					}
-					else if(command.equals("N")||command.equals("n")) {
-						System.out.println("Okay. You can now type in another command");
-					}
-				}
-				list.set(editNum,new Contact(c1,c2));
+                if(list.size() > 0) {
+                    System.out.println("Please select the number contact you want to edit:");
+                    for (String a : contacts) {
+                        System.out.println(a); //prints every element in contacts[]
+                    }
+                    editNum = scan.nextInt();
+				    System.out.println(list.get(editNum).toString());
+				    System.out.println("Do you need to edit the username? Y/N");
+				    if(command.equals("Y")||command.equals("y")) {
+					    System.out.println("Type in new name:");
+					    c1= scan.nextLine();
+				    }
+				    else if(command.equals("N")||command.equals("n")) {
+					    System.out.println("Do you need to edit the phone number? Y/N");
+					    if(command.equals("Y")||command.equals("y")) {
+					    	System.out.println("Type in new phone number");
+                            c2 = scan.nextInt();
+                            list.set(editNum,new Contact(c1,c2));
+                            System.out.println('\n' + "Your new contact information is: " + '\n' + '#' + editNum + "Contact Name: " + c1 + "Contact Phone Number: " + c2 );
+					    }
+					    else if(command.equals("N")||command.equals("n")) {
+						    System.out.println('\n'+"Exiting editor. . .");
+					    }
+				    }
+                    
+                }
+                else {
+                    System.out.println('\n'+"You need to add a contact to your list");
+                }
             }
-			else {
-				System.out.println("Invalid Input");
-			}
-            System.out.println('\n' + "Enter Command:");
+            //System.out.println('\n' + "Enter Command:");
             command = scan.nextLine();
         }
             System.exit(0);
-	}
+    }
+    
 }
+
+
